9e54f954a0b37b2d045f384768cda9f58a8efb44
Luke Mason <s3630120@student.rmit.edu.au>
2023-09-10 21:12:03 +1000

    made it use pip3

--- setup_luke_pc.sh
+++ setup_luke_pc.sh
@@ -1,2 +1,2 @@
-ansible-playbook -i inventory/luke_pc.ini playbook.yml --connection=local --ask-vault-pass --tag role_bootstrap
+ansible-playbook -i inventory/luke_pc.ini playbook.yml --connection=local --ask-vault-pass --ask-become-pass
 
